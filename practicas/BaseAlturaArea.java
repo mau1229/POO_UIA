47f59c2b93d04e435b5a1745b1d876f968229700
Mauricio Nieto Sainz <mauricio291295@live.com>
2016-01-29 00:05:57 -0600

    Estracturas de Repositorio

---
+++ practicas/BaseAlturaArea.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class BaseAlturaArea{
+
+public static void main (String[]args){
+
+             int area;
+
+             int altura;
+
+             int base;
+
+                         altura=100;
+
+                        base=20;
+
+                         area=(base*altura)/2;
+
+   System.out.println("el resultado es:"+ area);
+
+  }
+
+ }
